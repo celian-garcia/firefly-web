7c232c223fa65a6cd697ec2216da090250b3c473
Celian Garcia <celian.garcia1@gmail.com>
2018-02-22 09:04:38 +0100

    close the app-task-view when a flush is done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import {ToolbarButtonService} from './toolbar/toolbar-button.service';
 import {TaskMetadata} from 'app/api-firefly/data/TaskMetadata';
 import {TaskService} from "./api-firefly/task.service";
 import {ModuleService} from "./api-firefly/module.service";
+import {isNullOrUndefined} from "util";
 
 @Component({
     selector: 'app-root',
@@ -23,8 +24,8 @@ export class AppComponent implements OnInit {
         this.taskSelected = false;
     }
 
-    updateTask(event) {
-        this.taskSelected = true;
+    updateTaskId(event) {
+        this.taskSelected = !isNullOrUndefined(event);
         this.taskId = event;
     }
 }
